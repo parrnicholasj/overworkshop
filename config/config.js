0e8a0fee369884fea038c936c1b75fcd5b4ef743
parrn1 <mrnickp@comcast.net>
2019-05-14 13:15:35 -0400

    added sequelize db stuff

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
-{
+module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
+    "username": "bu9lgvf1t4y3nxlg",
+    "password": process.env.db_PW,
+    "database": "zefmahvdp9uys0h2",
+    "host": "mna97msstjnkkp7h.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     "dialect": "mysql"
   },
   "test": {
